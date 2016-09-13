15e4592e206c5dc1da51b3956237598015739127
Fritz <fre.healy@gmail.com>
2016-09-13 14:18:30 -0400

    major styling homepage - removing readme

--- index.php
+++ index.php
@@ -18,7 +18,10 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) :
+        $post = get_post(51);
+        setup_postdata($post);
+        
+		if ( have_posts() ) : the_post();
 
 			if ( is_home() && ! is_front_page() ) : ?>
 				<header>
@@ -28,20 +31,8 @@ get_header(); ?>
 			<?php
 			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
+            get_template_part( 'template-parts/content', 'index');
+				
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
@@ -52,5 +43,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
---
+++ template-parts/content-index.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<section class="row-1">
+        <?php if(get_field("row_1_image")):?>
+            <img class="full-width" src="<?php echo wp_get_attachment_image_src(get_field("row_1_image"),"full")[0];?>" alt="<?php echo get_post("row_1_image")!==null?get_post(get_field("row_1_image"))->post_title:"";?>">
+         <?php endif;//if for row_1_image?>
+        <header>
+            <h1><?php echo get_bloginfo("name");?></h1>
+        </header>
+	</section><!--.row-1-->
+	<section class="row-2">
+        <?php if(get_field("row_2_text")):?>
+            <div class="column-1"><?php echo get_field("row_2_text");?></div><!--.column-1-->
+        <?php endif;//if for row_2_text?>
+        <?php if(get_field("row_2_button_text")):?>
+            <div class="column-2"><button><?php echo get_field("row_2_button_text");?></button></div><!--.column-2-->
+        <?php endif;//if for row_2_button_text?>
+	</section><!--.row-2-->
+	<section class="row-3">
+        <div class="column-1">
+            <?php if(get_post(5)!==null):
+                $post=get_post(5);
+                setup_postdata($post);?>
+                <header>
+                    <h2><?php echo get_the_title();?></h2>
+                </header>
+                <div class="copy">
+                    <?php $the_content = get_the_content();
+                    if(strlen($the_content)<=200):
+                        echo $the_content;
+                    else: 
+                        echo substr($the_content,0,200)."...";
+                    endif;?>
+                </div><!--.copy-->
+            <?php endif;//if for get_post?>
+        </div><!--.column-1-->
+        <div class="column-2">
+            <?php if(get_post(53)!==null):
+                $post= get_post(53);
+                setup_postdata($post);?>
+                <header>
+                    <h2><?php echo get_the_title();?></h2>
+                </header>
+                <div class="copy">
+                    <?php $the_content = get_the_content();
+                    if(strlen($the_content)<=200):
+                        echo $the_content;
+                    else: 
+                        echo substr($the_content,0,200)."...";
+                    endif;?>
+                </div><!--.copy-->
+            <?php endif;//if for get_post?>
+        </div><!--.column-2-->
+        <div class="column-3">
+            <?php if(get_post(55)!==null):
+                $post=get_post(55);
+                setup_postdata($post);?>
+                <header>
+                    <h2><?php echo get_the_title();?></h2>
+                </header>
+                <div class="copy">
+                    <?php $the_content = get_the_content();
+                    if(strlen($the_content)<=200):
+                        echo $the_content;
+                    else: 
+                        echo substr($the_content,0,200)."...";
+                    endif;?>
+                </div><!--.copy-->
+            <?php endif;//if for get_post?>
+        </div><!--.column-3-->
+	</section><!--.row-3-->
+</article><!-- #post-## -->
