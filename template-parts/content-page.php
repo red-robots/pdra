abcb641c8b730f8c469da911cd84c5e9ab1b0a46
Fritz <fre.healy@gmail.com>
2016-09-22 16:23:34 -0400

    major and minor styling for page.php complete

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
     <section class="row-1 standard-sub-nav">
 		<?php $matches = array();
 		$title = false;
@@ -21,6 +21,7 @@
             <header><h1><?php echo get_the_title();
             $title = true;?></h1></header>
         <?php endif; ?>
+        <div class="background-image" style="background-image:url(<?php if(get_field("standard_nav_background_image","option")) echo wp_get_attachment_image_src(get_field("standard_nav_background_image","option"),"full")[0];?>);"></div>
     </section>
 	<section class="row-2">
         <?php if(!$title):?>
